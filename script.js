9c6d36fd51bf3813b68944948cf55a97cc98b6c8
ViniRivaldar <vinicius-rivaldar@outlook.com>
2024-02-20 11:43:58 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger)
+const tl = gsap.timeline({
+    scrollTrigger:{
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+        ease: "linear"
+    }
+})
+
+tl.to('.acordion .text', {
+    height: 0,
+    paddingBottom: 0,
+    opacity: 0,
+    stagger: .5
+})
+tl.to('.acordion',{
+    marginBottom: -15,
+    stagger: .5
+})
